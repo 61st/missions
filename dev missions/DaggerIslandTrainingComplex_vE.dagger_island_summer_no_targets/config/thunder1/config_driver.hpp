343511f2eec123a7ec1133408de4e9f96bbe8f02
YonV <32718226+YonVclaw@users.noreply.github.com>
2024-10-31 06:30:40 -0600

    fixes

--- dev missions/DaggerIslandTrainingComplex_vE.dagger_island_summer_no_targets/config/thunder1/config_driver.hpp
+++ dev missions/DaggerIslandTrainingComplex_vE.dagger_island_summer_no_targets/config/thunder1/config_driver.hpp
@@ -13,7 +13,7 @@ class driver1 {
     defaultLoadout[] = {
             {"lxim_sig_XM7","KAR_XM250_SUP","rhsusf_acc_anpeq15","lxim_equipment_optic_DMS",{"lxim_sig_25Rnd_Fury",25},{},""},{},
             {"MHS_M17","","","",{"MHS_21rnd_9MM_124FMJ_M17",21},{},""},
-            {"U_tweed_acu_summer_ocp",{{"ACE_EarPlugs",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"ACRE_PRC148",1},{"",1,0}}},
+            {"U_tweed_acu_summer_ocp",{{"ACE_EarPlugs",1},{"ItemcTabHCam",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"ACRE_PRC148",1},{"",1,0}}},
             {"V_tweed_msv_mk2_1",{{"ACE_CableTie",4},{"ACE_microDAGR",1},{"ACE_Flashlight_MX991",1},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"SmokeShell",2,1},{"HandGrenade",2,1},{"lxim_sig_25Rnd_Fury",12,25}}},
             {"B_simc_US_Molle_sturm_OCP",{{"ACE_EntrenchingTool",1},{"ACE_IR_Strobe_Item",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"psq42_blk",1}}},
             "H_tweed_ihps_g_tasc_rail","G_oak_2_cut",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
