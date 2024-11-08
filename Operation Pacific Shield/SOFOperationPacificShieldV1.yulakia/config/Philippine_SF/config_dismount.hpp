0f89e3302b42ab50012de557bc2a66841fa4b12f
whiteytidys <88219511+whiteytidys@users.noreply.github.com>
2024-11-08 14:29:45 -0600

    SFMap Changes and Philippine SF Added 11824

---
+++ Operation Pacific Shield/SOFOperationPacificShieldV1.yulakia/config/Philippine_SF/config_dismount.hpp
@@ -0,0 +1,66 @@
+class dismountpsf {
+    name = "dismount";
+    description = "Ground and pound...";
+    traits[] = {
+
+    };
+    customVariables[] = {
+        {"TFAR_freq_sr",{"100.1","110","110.2","33","100","75","60","35"},true}
+    };
+    icon = "z\LXIM\addons\media\images\Icons\iconman_red.paa";
+
+    defaultLoadout[] = {
+            {"cup_arifle_hk416_wood","CUP_muzzle_snds_G36_wood","","CUP_optic_Elcan_reflex_OD",{"CUP_30Rnd_556x45_Emag",30},{},""},{},
+            {"CUP_hgun_M9A1","CUP_muzzle_snds_M9","CUP_acc_Glock17_Flashlight","",{"CUP_15Rnd_9x19_M9",15},{},""},
+            {"U_I_CombatUniform",{{"ACE_EarPlugs",2},{"ACE_CableTie",4},{"ACE_IR_Strobe_Item",1},{"ACE_SpraypaintGreen",1},{"ace_marker_flags_green",4},{"ACE_EntrenchingTool",1},{"ACE_Flashlight_MX991",1},{"ACE_Clacker",1},{"ACE_DefusalKit",1}}},
+            {"V_CarrierRigKBT_01_light_Olive_F",{{"CUP_30Rnd_556x45_Emag",11,30},{"CUP_15Rnd_9x19_M9",3,15},{"CUP_HandGrenade_M67",2,1},{"SmokeShell",1,1}}},
+            {"lxim_backpack_TacticalPack_Multicam_Woodland",{{"kat_AFAK",1},{"tsp_breach_block_mag",4,1},{"SmokeShellGreen",1,1},{"SmokeShellBlue",1,1},{"SmokeShellRed",1,1}}},
+            "CUP_H_OpsCore_Covered_AAF_SF","",{"Binocular","","","",{},{},""},
+            {"ItemMap","ItemMicroDAGR","TFAR_anprc152","ItemCompass","ItemWatch","CUP_NVG_GPNVG_green"}
+    };
+    // for 1-1
+    arsenalitems[] = {
+        "cup_glaunch_mk13",
+        "CUP_FlareYellow_M203",
+        "CUP_1Rnd_HE_M203",
+        "CUP_1Rnd_HEDP_M203",
+        "CUP_FlareRed_M203",
+        "CUP_1Rnd_StarCluster_Green_M203",
+        "CUP_1Rnd_StarCluster_Red_M203",
+        "CUP_FlareWhite_M203",
+        "CUP_1Rnd_Smoke_M203",
+        "CUP_1Rnd_StarFlare_Red_M203",
+        "CUP_FlareGreen_M203",
+        "CUP_1Rnd_StarFlare_White_M203",
+        "CUP_1Rnd_StarFlare_Green_M203",
+        "CUP_1Rnd_SmokeGreen_M203",
+        "ACE_HuntIR_M203",
+        "CUP_1Rnd_StarCluster_White_M203",
+        "CUP_1Rnd_SmokeRed_M203",
+        "CUP_1Rnd_SmokeYellow_M203",
+        "CUP_1Rnd_HE_M203",
+        "CUP_1Rnd_HEDP_M203",
+        "CUP_FlareRed_M203",
+        "CUP_1Rnd_StarCluster_Green_M203",
+        "CUP_1Rnd_StarCluster_Red_M203",
+        "CUP_FlareWhite_M203",
+        "CUP_1Rnd_Smoke_M203",
+        "CUP_1Rnd_StarFlare_Red_M203",
+        "CUP_FlareGreen_M203",
+        "CUP_1Rnd_StarFlare_White_M203",
+        "CUP_1Rnd_StarFlare_Green_M203",
+        "CUP_1Rnd_SmokeGreen_M203",
+        "ACE_HuntIR_M203",
+        "CUP_1Rnd_StarCluster_White_M203",
+        "CUP_1Rnd_SmokeRed_M203",
+        "CUP_1Rnd_SmokeYellow_M203",
+        "ACE_LWAMG_SpareBarrel",
+        "ACE_LAMG_SpareBarrel",
+        "lxim_sig_M250",
+        "lxim_sig_M250_BLK",
+        "CUP_sgun_M1014_Entry",
+        "CUP_sgun_M1014_Entry_vfg",
+        "muzzle_snds_M",
+        "CUP_muzzle_snds_M110_black"
+    };
+};
