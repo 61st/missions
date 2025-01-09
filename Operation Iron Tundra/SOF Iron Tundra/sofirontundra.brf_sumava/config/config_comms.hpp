2add0b37821edca49105e7c3b9a8a211a3810df0
whiteytidys <88219511+whiteytidys@users.noreply.github.com>
2025-01-08 21:37:02 -0600

    WIP01082025Curysof

---
+++ Operation Iron Tundra/SOF Iron Tundra/sofirontundra.brf_sumava/config/config_comms.hpp
@@ -0,0 +1,67 @@
+private _srHashMap = [
+    
+    ["unassigned",          [["ACRE", 7], ["TFAR", 50]]],
+    ["OUTLAW-HQ",           [["ACRE", 1], ["TFAR", 50]]],
+    ["OUTLAW-1/1",          [["ACRE", 2], ["TFAR", 121.1]]],
+    ["OUTLAW-1/2",          [["ACRE", 4], ["TFAR", 121.2]]],
+
+    ["RECRUITS",            [["ACRE", 8], ["TFAR", 52]]],
+    ["STUDENTS",            [["ACRE", 8], ["TFAR", 52]]],
+
+    ["RHINO",               [["ACRE", 7], ["TFAR", 52]]],
+
+    ["WARMOMMY-2/1",        [["ACRE", 9], ["TFAR", 52.1]]],
+    ["WARCHILD-2/2",        [["ACRE", 10], ["TFAR", 52.1]]],
+    ["WARDADDY-2/6",        [["ACRE", 11], ["TFAR", 52.1]]],
+
+    ["PHOENIX",             [["ACRE", 12], ["TFAR", 52]]],
+    ["PHOENIX-3/1",         [["ACRE", 13], ["TFAR", 52]]],
+    ["PHOENIX-3/2",         [["ACRE", 14], ["TFAR", 52]]],
+    ["PHOENIX-3/3",         [["ACRE", 15], ["TFAR", 52]]]
+];
+
+private _mrHashMap = [
+    ["OUTLAW-HQ",           [["ACRE", 1], ["TFAR", 50]]],
+    ["OUTLAW-1/1",          [["ACRE", 2], ["TFAR", 121.1]]],
+    ["OUTLAW-1/2",          [["ACRE", 3], ["TFAR", 121.2]]],
+
+    ["RECRUITS",            [["ACRE", 14], ["TFAR", 52]]],
+    ["STUDENTS",            [["ACRE", 15], ["TFAR", 52]]],
+
+    //Air   
+    ["PHOENIX",             [["ACRE", 7], ["TFAR", 52]]],
+    ["PHOENIX-3/1",         [["ACRE", 7], ["TFAR", 52]]],
+    ["PHOENIX-3/2",         [["ACRE", 7], ["TFAR", 52]]],
+    ["PHOENIX-3/3",         [["ACRE", 7], ["TFAR", 52]]],
+
+    ["RHINO",               [["ACRE", 6], ["TFAR", 52]]],
+
+    ["WARMOMMY-2/1",        [["ACRE", 6], ["TFAR", 52.1]]],
+    ["WARCHILD-2/2",        [["ACRE", 6], ["TFAR", 52.1]]],
+    ["WARDADDY-2/6",        [["ACRE", 6], ["TFAR", 52.1]]],
+
+    ["RANGE-CONTROL",       [["ACRE", 18], ["TFAR", 50]]]
+];
+
+private _lrHashMap = [
+    ["OUTLAW-HQ",           [["ACRE", 4], ["TFAR", 50]]],
+    ["OUTLAW-1/1",          [["ACRE", 1], ["TFAR", 121.1]]],
+    ["OUTLAW-1/2",          [["ACRE", 1], ["TFAR", 121.2]]],
+
+    ["RECRUITS",            [["ACRE", 6], ["TFAR", 52]]],
+    ["STUDENTS",            [["ACRE", 6], ["TFAR", 52]]],
+
+    //Air   
+    ["PHOENIX",             [["ACRE", 4], ["TFAR", 52]]],
+    ["PHOENIX-3/1",         [["ACRE", 10], ["TFAR", 52]]],
+    ["PHOENIX-3/2",         [["ACRE", 10], ["TFAR", 52]]],
+    ["PHOENIX-3/3",         [["ACRE", 10], ["TFAR", 52]]],
+
+    ["RHINO",               [["ACRE", 4], ["TFAR", 52]]],
+
+    ["WARMOMMY-2/1",        [["ACRE", 2], ["TFAR", 52.1]]],
+    ["WARCHILD-2/2",        [["ACRE", 3], ["TFAR", 52.1]]],
+    ["WARDADDY-2/6",        [["ACRE", 4], ["TFAR", 52.1]]],
+
+    ["RANGE-CONTROL",       [["ACRE", 4], ["TFAR", 50]]]
+];
