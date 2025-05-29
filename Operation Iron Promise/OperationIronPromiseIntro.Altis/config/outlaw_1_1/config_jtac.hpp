b44c1346f963e725a8ac2371219294c855bef1bd
whiteytidys <whiteytidys@gmail.com>
2025-05-28 21:35:20 -0500

    missionedits052825

--- Operation Iron Promise/OperationIronPromiseIntro.Altis/config/outlaw_1_1/config_jtac.hpp
+++ Operation Iron Promise/OperationIronPromiseIntro.Altis/config/outlaw_1_1/config_jtac.hpp
@@ -16,11 +16,11 @@ class jtac11 {
     defaultLoadout[] = {
             {"lxim_sig_XM7","ICM_SLX_Suppressor_Arid_F","rhsusf_acc_anpeq15side","lxim_equipment_optic_DMS",{"lxim_sig_25Rnd_Fury",25},{},""},{},
             {"MHS_M17","","","",{"MHS_21rnd_9MM_124FMJ_M17",21},{},""},
-            {"U_tweed_acu_summer_ocp",{{"ACE_EarPlugs",1},{"ItemcTabHCam",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"ACRE_PRC148",1},{"",1,0}}},
+            {"U_tweed_acu_summer_ocp",{{"ACE_EarPlugs",1},{"ItemcTabHCam",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"",1,0}}},
             {"V_tweed_msv_mk2_1",{{"ACE_CableTie",4},{"ACE_microDAGR",1},{"tun_tablet",1},{"ACE_Flashlight_MX991",1},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"SmokeShell",2,1},{"HandGrenade",2,1},{"lxim_sig_25Rnd_Fury",12,25}}},
             {"B_simc_US_Molle_sturm_OCP_RTO_wasser",{{"ACRE_PRC117F",1},{"ACE_EntrenchingTool",1},{"MS_Strobe_Mag_1",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"psq42_blk",1}}},
             "H_tweed_ihps_g_tasc_rail","G_oak_2_cut",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
-            {"ItemMap","ItemAndroid","","ItemCompass","ACE_Altimeter",""}
+            {"ItemMap","ItemAndroid","TFAR_anprc152_1","ItemCompass","ACE_Altimeter",""}
     };
     arsenalitems[] = {
         "tun_tablet",
@@ -39,6 +39,7 @@ class jtac11 {
         "lxim_vs17_vs17",
         "ACRE_VHF30108SPIKE",
         "ACRE_VHF30108",
-        "ACRE_VHF30108MAST"
+        "ACRE_VHF30108MAST",
+        "rhs_weap_M320"
     };
 };
--- training/DaggerIslandTrainingComplex_vE.dagger_island_summer_no_targets/config/training/config_jtac.hpp
+++ training/DaggerIslandTrainingComplex_vE.dagger_island_summer_no_targets/config/training/config_jtac.hpp
@@ -20,7 +20,7 @@ class jtacs {
             {"V_tweed_msv_mk2_1",{{"ACE_CableTie",4},{"ACE_microDAGR",1},{"tun_tablet",1},{"ACE_Flashlight_MX991",1},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"SmokeShell",2,1},{"HandGrenade",2,1},{"lxim_sig_25Rnd_Fury",12,25}}},
             {"B_simc_US_Molle_sturm_OCP_RTO_wasser",{{"ACRE_PRC117F",1},{"ACE_EntrenchingTool",1},{"MS_Strobe_Mag_1",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"psq42_blk",1}}},
             "H_tweed_ihps_g_tasc_rail","G_oak_2_cut",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
-            {"ItemMap","ItemAndroid","","ItemCompass","ACE_Altimeter",""}
+            {"ItemMap","ItemAndroid","TFAR_anprc152_1","ItemCompass","ACE_Altimeter",""}
     };
     arsenalitems[] = {
         "tun_tablet",
@@ -39,6 +39,7 @@ class jtacs {
         "lxim_vs17_vs17",
         "ACRE_VHF30108SPIKE",
         "ACRE_VHF30108",
-        "ACRE_VHF30108MAST"
+        "ACRE_VHF30108MAST",
+        "rhs_weap_M320"
     };
 };
