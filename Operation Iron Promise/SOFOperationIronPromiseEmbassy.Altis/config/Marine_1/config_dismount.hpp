a3398a948bd029b34865a5f9ef5772e3201952d3
whiteytidys <whiteytidys@gmail.com>
2025-05-30 17:06:34 -0500

    sideopedits53025

---
+++ Operation Iron Promise/SOFOperationIronPromiseEmbassy.Altis/config/Marine_1/config_dismount.hpp
@@ -0,0 +1,39 @@
+class dismount11 {
+    name = "Embassy Guard";
+    description = "Its a grunts life";
+    traits[] = {};
+    customVariables[] = {
+        {"ace_medical_medicClass",0,true},
+        {"ace_isEngineer",0,true}
+    };
+    icon = "z\LXIM\addons\media\images\Icons\iconman_red.paa";
+
+    defaultLoadout[] = {
+            {"rhs_weap_m16a4_carryhandle","","","",{"rhs_mag_30Rnd_556x45_M855A1_Stanag",30},{},""},{},
+            {"rhsusf_weap_m9","","","",{"rhsusf_mag_15Rnd_9x19_JHP",15},{},""},
+            {"rhs_uniform_FROG01_wd",{{"lxim_medbags_FirstAid",1},{"ACE_EarPlugs",1},{"ACE_IR_Strobe_Item",1},{"ItemcTabHCam",1}}},
+            {"rhsusf_spc_squadleader",{{"rhsusf_mag_15Rnd_9x19_FMJ",4,15},{"tsp_flashbang_m84",2,1},{"rhs_mag_30Rnd_556x45_M855_Stanag_Pull",11,30}}},
+            {"lxim_backpack_AssaultPackEnhanced_Solid_CoyoteBrown",{{"ACE_EntrenchingTool",1},{"ACE_M84",2,1},{"HandGrenade",1,1},{"cigs_lighter",1,100},{"cigs_morley_cigpack",1,20}}},
+            "rhsusf_mich_helmet_marpatwd_alt_headset","rhs_googles_clear",{"Binocular","","","",{},{},""},
+            {"ItemMap","ItemMicroDAGR","TFAR_anprc152","ItemCompass","ItemWatch","rhsusf_ANPVS_14"}
+    };
+    // for 1-1
+    arsenalitems[] = {
+        "ACE_HuntIR_M203",
+        "ACE_LAMG_SpareBarrel",
+        "ACE_LWAMG_SpareBarrel",
+        "rhs_weap_M320",
+        "rhsusf_acc_ARDEC_M240",
+        "UGL_FlareGreen_F",
+        "rhs_weap_m240B",
+        "rhsusf_100Rnd_762x51",
+        "rhs_weap_m240B",
+        "rhsusf_100Rnd_762x51",
+        "rhs_weap_m249_pip",
+        "rhsusf_200rnd_556x45_M855_mixed_box",
+        "rhsusf_200rnd_556x45_M855_box",
+        "rhsusf_100Rnd_556x45_M855_mixed_soft_pouch_coyote",
+        "rhsusf_100Rnd_556x45_M855_soft_pouch_ucp",
+        "rhs_weap_m16a4"
+    };
+};
