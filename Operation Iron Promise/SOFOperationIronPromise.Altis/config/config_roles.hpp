e83ab06d78ad98c17d636dbf8efc7ba251a56538
whiteytidys <whiteytidys@gmail.com>
2025-06-12 18:27:39 -0500

    SFworkcompleted

--- Operation Iron Promise/SOFOperationIronPromise.Altis/config/config_roles.hpp
+++ Operation Iron Promise/SOFOperationIronPromise.Altis/config/config_roles.hpp
@@ -6,11 +6,6 @@ class Dynamic_Roles {
     #include "outlaw_hq\config_engineer.hpp"
     #include "outlaw_hq\config_mark.hpp"
 
-    #include "thunder\config_cmd.hpp"
-    #include "thunder\config_driver.hpp"
-    #include "thunder\config_gunner.hpp"
-    #include "thunder\config_medic.hpp"
-
     #include "outlaw_1_1\config_engineer.hpp"
     #include "outlaw_1_1\config_squadleader.hpp"
     #include "outlaw_1_1\config_teamleader.hpp"
@@ -18,15 +13,18 @@ class Dynamic_Roles {
     #include "outlaw_1_1\config_jtac.hpp"
     #include "outlaw_1_1\config_dismount.hpp"
 
+    #include "sealteam_1\config_seal.hpp"
+    #include "sealteam_1\config_sealmedic.hpp"
+
+    #include "sealteam_2\config_seal.hpp"
+    #include "sealteam_2\config_sealmedic.hpp"
+
     #include "outlaw_1_2\config_engineer.hpp"
     #include "outlaw_1_2\config_squadleader.hpp"
     #include "outlaw_1_2\config_teamleader.hpp"
     #include "outlaw_1_2\config_medic.hpp"
     #include "outlaw_1_2\config_jtac.hpp"
     #include "outlaw_1_2\config_dismount.hpp"
-
-    #include "rhino\config_squadleader.hpp"
-    #include "rhino\config_engineer.hpp"
 
     #include "phoenix\config_pilot.hpp"
     #include "phoenix\config_crewchief.hpp"
