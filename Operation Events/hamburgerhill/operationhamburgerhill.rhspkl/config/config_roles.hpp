917af23fd1e778b111bc60feb7e4e0dd738b7737
whiteytidys <whiteytidys@gmail.com>
2025-08-01 19:09:06 -0500

    curyvietnam_8125

---
+++ Operation Events/hamburgerhill/operationhamburgerhill.rhspkl/config/config_roles.hpp
@@ -0,0 +1,55 @@
+class Dynamic_Roles {
+    #include "outlaw_hq\config_co.hpp"
+    #include "outlaw_hq\config_xo.hpp"
+    #include "outlaw_hq\config_medic.hpp"
+    #include "outlaw_hq\config_jtac.hpp"
+    #include "outlaw_hq\config_engineer.hpp"
+    #include "outlaw_hq\config_mark.hpp"
+
+    #include "outlaw_1_1\config_engineer.hpp"
+    #include "outlaw_1_1\config_squadleader.hpp"
+    #include "outlaw_1_1\config_teamleader.hpp"
+    #include "outlaw_1_1\config_medic.hpp"
+    #include "outlaw_1_1\config_jtac.hpp"
+    #include "outlaw_1_1\config_dismount.hpp"
+
+    #include "sealteam_1\config_seal.hpp"
+    #include "sealteam_1\config_sealmedic.hpp"
+
+    #include "sealteam_2\config_seal.hpp"
+    #include "sealteam_2\config_sealmedic.hpp"
+
+    #include "sogarmy\config_sogarmy.hpp"
+    #include "sogarmy\config_sogarmymedic.hpp"
+
+    #include "sofnkorreno1\config_nkor.hpp"
+    #include "sofnkorreno1\config_nkormedic.hpp"
+
+    #include "sofnkorreno2\config_nkor.hpp"
+    #include "sofnkorreno2\config_nkormedic.hpp"
+
+    #include "cia\config_ciaop.hpp"
+
+    #include "banshiri_ng\config_banshiring.hpp"
+    #include "banshiri_ng\config_banshiringmed.hpp"
+
+    #include "outlaw_1_2\config_engineer.hpp"
+    #include "outlaw_1_2\config_squadleader.hpp"
+    #include "outlaw_1_2\config_teamleader.hpp"
+    #include "outlaw_1_2\config_medic.hpp"
+    #include "outlaw_1_2\config_jtac.hpp"
+    #include "outlaw_1_2\config_dismount.hpp"
+
+    #include "phoenix\config_pilot.hpp"
+    #include "phoenix\config_crewchief.hpp"
+    #include "phoenix\config_medic.hpp"
+    #include "phoenix\config_jtac.hpp"
+
+    #include "training\config_cadre.hpp"
+    #include "training\config_dismount.hpp"
+    #include "training\config_engineer.hpp"
+    #include "training\config_jtac.hpp"
+    #include "training\config_medic.hpp"
+    #include "training\config_zeus.hpp"
+
+};
