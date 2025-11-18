0b65ad06add7fd60bd70cfb43a59712f3399e6f0
whiteytidys <whiteytidys@gmail.com>
2025-11-17 18:40:31 -0600

    61stgokart_cury_111725

---
+++ Operation Events/61stgokarts.Altis/config/config_arsenal.hpp
@@ -0,0 +1,27 @@
+class Common_Arsenal {
+    items[] = {
+        "TFAR_anprc152_1",
+        "U_C_Driver_1_black",
+        "U_C_Driver_1_blue",
+        "U_C_Driver_2",
+        "U_C_Driver_1",
+        "U_C_Driver_1_green",
+        "U_C_Driver_1_orange",
+        "U_C_Driver_1_red",
+        "U_C_Driver_3",
+        "U_C_Driver_4",
+        "U_C_Driver_1_white",
+        "U_C_Driver_1_yellow",
+        "H_RacingHelmet_1_blue_F",
+        "H_RacingHelmet_2_F",
+        "H_RacingHelmet_1_F",
+        "H_RacingHelmet_1_green_F",
+        "H_RacingHelmet_1_orange_F",
+        "H_RacingHelmet_1_red_F",
+        "H_RacingHelmet_3_F",
+        "H_RacingHelmet_1_white_F",
+        "H_RacingHelmet_4_F",
+        "H_RacingHelmet_1_yellow_F"
+    };
+};
+
---
+++ Operation Events/61stgokarts.Altis/config/config_groups.hpp
@@ -0,0 +1,24 @@
+class Dynamic_Groups { //format: {"Group Name",{"Roles"},"Conditions for the group to be shown"}
+    faction_name = "61st";
+    group_setup[] = {
+        {"OUTLAW-1/1",{"dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11"},"true"},
+        {"OUTLAW-1/2",{"dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11","dismount11"},"true"},
+        //{"THUNDER-2/1",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/2",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/3",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/6",{"cmd","driver","gunner"},"true"},
+        //{"OUTLAW-1/6",{"co","xo","jtac","medic","engineer","mark","mark"},"true"},
+        //{"RRR",{"squadleadr","engineerr","medict"},"true"},
+        //{"RHINO",{"squadleadr","engineerr","engineerr","engineerr","engineerr","engineerr","engineerr"},"true"},
+        //{"PHOENIX-3/6",{"pilot","pilot","fac"},"true"},
+        //{"PHOENIX-3/1",{"pilot","pilot","crewchief","crewchief","medicp"},"true"},
+        //{"PHOENIX-3/2",{"pilot","pilot","crewchief","crewchief","medicp"},"true"},
+        //{"PHOENIX-3/3",{"pilot","pilot","crewchief","crewchief","medicp"},"true"},
+        //{"RECRUITS",{"Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit"},"true"},
+        {"RANGE-CTL",{"Zeus","Zeus"},"true"}
+    };
+};
+
+#include "config_roles.hpp"
+#include "config_arsenal.hpp"
+
