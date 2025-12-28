e73cb06f183ce4850f2cdb826fb2dc5a3c223ee2
whiteytidys <whiteytidys@gmail.com>
2025-12-28 14:20:29 -0600

    61st_newcampaign_122825

---
+++ Operation Black Eagle/operation_blackeaglesf_v1.VTF_Korsac/config/config_groups.hpp
@@ -0,0 +1,32 @@
+class Dynamic_Groups { //format: {"Group Name",{"Roles"},"Conditions for the group to be shown"}
+    faction_name = "61st";
+    group_setup[] = {
+        //{"OUTLAW-1/1",{"squadlead11","jtac11","engineer11","medic11","teamlead11","dismount11","dismount11","dismount11","dismount11","dismount11"},"true"},
+        //{"OUTLAW-1/2",{"squadlead12","jtac12","engineer12","medic12","teamlead12","dismount12","dismount12","dismount12","dismount12","dismount12"},"true"},
+        {"Seal-Team-1",{"sealteam1","sealteam1","sealteam1","sealmed1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1"},"true"},
+        {"Seal Team-2",{"sealteam2","sealteam2","sealteam2","sealmed2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2"},"true"},
+        //{"CIA Agent",{"ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop"},"true"},
+        //{"Banshiri NG",{"banshiring","banshiring","banshiring","banshiringmed","banshiringmed","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring"},"true"},
+        //{"Banshiri RG",{"banshirisf","banshirisf","banshirisf","banshirisfmed","banshirisfmed","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf"},"true"},
+        //{"OUTLAW-1/6",{"co","xo","jtac","medic","engineer","mark","mark"},"true"},
+        //{"PHOENIX-3/6",{"pilot","pilot","fac"},"true"},
+        //{"THUNDER-2/1",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/2",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/3",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/4",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/5",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/6",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/7",{"cmd","driver","gunner"},"true"},
+        //{"THUNDER-Extra",{"cmd","driver","gunner","cmd","driver","gunner","cmd","driver","gunner","cmd","driver","gunner"},"true"},
+        //{"THUNDER-2/ME",{"cmd","medict","repair"},"true"},
+        {"PHOENIX-3/1",{"pilot","pilot","crewchief","crewchief","medicp"},"true"},
+        //{"PHOENIX-3/2",{"pilot","pilot","crewchief","crewchief","medicp"},"true"},
+        //{"PHOENIX-3/3",{"pilot","pilot","crewchief","crewchief","medicp"},"true"},
+        //{"RECRUITS",{"Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit","Recruit"},"true"},
+        {"RANGE-CTL",{"Zeus","Zeus"},"true"}
+    };
+};
+
+#include "config_roles.hpp"
+#include "config_arsenal.hpp"
+
