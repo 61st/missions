5845abbbc0a54a3c183664fbfd9786747e9db10e
whiteytidys <whiteytidys@gmail.com>
2026-01-20 19:28:40 -0600

    mendezadditions

---
+++ Operation Black Eagle/operation_blackeaglesf_v2.archie/config/config_roles.hpp
@@ -0,0 +1,57 @@
+class Dynamic_Roles {
+    #include "outlaw_hq\config_co.hpp"
+    #include "outlaw_hq\config_xo.hpp"
+    #include "outlaw_hq\config_medic.hpp"
+    #include "outlaw_hq\config_jtac.hpp"
+    #include "outlaw_hq\config_engineer.hpp"
+    #include "outlaw_hq\config_mark.hpp"
+
+    #include "outlaw_1_1\config_engineer.hpp"
+    #include "outlaw_1_1\config_squadleader.hpp"
+    #include "outlaw_1_1\config_teamleader.hpp"
+    #include "outlaw_1_1\config_medic.hpp"
+    #include "outlaw_1_1\config_jtac.hpp"
+    #include "outlaw_1_1\config_dismount.hpp"
+
+    #include "sealteam_1\config_seal.hpp"
+    #include "sealteam_1\config_sealmark.hpp"
+    #include "sealteam_1\config_sealmedic.hpp"
+
+    #include "sealteam_2\config_seal.hpp"
+    #include "sealteam_2\config_sealmark.hpp"
+    #include "sealteam_2\config_sealmedic.hpp"
+
+    #include "cia\config_ciaop.hpp"
+
+    #include "banshiri_ng\config_banshiring.hpp"
+    #include "banshiri_ng\config_banshiringmed.hpp"
+
+    #include "banshiri_sf\config_banshirisf.hpp"
+    #include "banshiri_sf\config_banshirisfmed.hpp"
+
+    #include "outlaw_1_2\config_engineer.hpp"
+    #include "outlaw_1_2\config_squadleader.hpp"
+    #include "outlaw_1_2\config_teamleader.hpp"
+    #include "outlaw_1_2\config_medic.hpp"
+    #include "outlaw_1_2\config_jtac.hpp"
+    #include "outlaw_1_2\config_dismount.hpp"
+
+    #include "thunder\config_cmd.hpp"
+    #include "thunder\config_driver.hpp"
+    #include "thunder\config_gunner.hpp"
+    #include "thunder\config_medict.hpp"
+    #include "thunder\config_repair.hpp"
+
+    #include "phoenix\config_pilot.hpp"
+    #include "phoenix\config_crewchief.hpp"
+    #include "phoenix\config_medic.hpp"
+    #include "phoenix\config_jtac.hpp"
+
+    #include "training\config_cadre.hpp"
+    #include "training\config_dismount.hpp"
+    #include "training\config_engineer.hpp"
+    #include "training\config_jtac.hpp"
+    #include "training\config_medic.hpp"
+    #include "training\config_zeus.hpp"
+
+};
