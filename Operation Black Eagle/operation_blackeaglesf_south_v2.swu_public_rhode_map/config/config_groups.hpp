3378ad2aa4888a4cda0645286accaf7e099667cb
whiteytidys <whiteytidys@gmail.com>
2026-01-30 18:30:19 -0600

    filescleanedup_sfedits_ciaedits_cury_13026

--- Operation Black Eagle/operation_blackeaglesf_south_v2.swu_public_rhode_map/config/config_groups.hpp
+++ Operation Black Eagle/operation_blackeaglesf_south_v2.swu_public_rhode_map/config/config_groups.hpp
@@ -3,9 +3,9 @@ class Dynamic_Groups { //format: {"Group Name",{"Roles"},"Conditions for the gro
     group_setup[] = {
         //{"OUTLAW-1/1",{"squadlead11","jtac11","engineer11","medic11","teamlead11","dismount11","dismount11","dismount11","dismount11","dismount11"},"true"},
         //{"OUTLAW-1/2",{"squadlead12","jtac12","engineer12","medic12","teamlead12","dismount12","dismount12","dismount12","dismount12","dismount12"},"true"},
-        {"Seal-Team-1",{"sealteam1","sealteam1","sealteam1","sealmed1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteammark1"},"true"},
-        {"Seal Team-2",{"sealteam2","sealteam2","sealteam2","sealmed2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteammark1"},"true"},
-        //{"CIA Agent",{"ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop"},"true"},
+        //{"Seal-Team-1",{"sealteam1","sealteam1","sealteam1","sealmed1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteam1","sealteammark1"},"true"},
+        //{"Seal Team-2",{"sealteam2","sealteam2","sealteam2","sealmed2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteam2","sealteammark1"},"true"},
+        {"CIA Agent",{"ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciaop","ciamedic","ciamedic","ciamarks","ciamarks"},"true"},
         //{"Banshiri NG",{"banshiring","banshiring","banshiring","banshiringmed","banshiringmed","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring","banshiring"},"true"},
         //{"Banshiri RG",{"banshirisf","banshirisf","banshirisf","banshirisfmed","banshirisfmed","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf","banshirisf"},"true"},
         //{"OUTLAW-1/6",{"co","xo","jtac","medic","engineer","mark","mark"},"true"},
