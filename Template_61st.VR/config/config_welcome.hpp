1be9c83c7f4b2988162873e710f18aef9e2e0bda
whiteytidys <whiteytidys@gmail.com>
2026-02-02 20:17:42 -0600

    61st_vrmissionupdate_2226_cury

---
+++ Template_61st.VR/config/config_welcome.hpp
@@ -0,0 +1,12 @@
+private _message = "
+	<t align='center' size='5' shadow='0'><br/>Your Operation</t><br/>
+	Welcome to the 61st Ops Server<br/>
+	To get started, select a role from the group menu and access subsequent menus through ACE Self Interact. You can view more information in the diary.<br/><br/>
+	<br/>
+	Info Box
+	<br/>
+	<br/>
+	There are signs with instructions around the starting base
+	<br/><br/>
+	<a color='#00DFFF'>•</a> Discord: <a href='https://discord.gg/61st-mechanized' color='#56BDD6'> https://discord.gg/61st-mechanized</a>
+";
