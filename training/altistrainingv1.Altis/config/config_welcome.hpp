f97f9cf8411455177032daa19245581bae25b0af
whiteytidys <whiteytidys@gmail.com>
2026-03-21 22:14:53 -0500

    altiscorrections_cury_32126

--- training/altistrainingv1.Altis/config/config_welcome.hpp
+++ training/altistrainingv1.Altis/config/config_welcome.hpp
@@ -1,10 +1,9 @@
 private _message = "
-	<t align='center' size='5' shadow='0'><br/>Operation Black Eagle</t><br/>
-	Welcome to the 61st Ops Server<br/>
+	<t align='center' size='5' shadow='0'><br/>Altis Training Island</t><br/>
+	Welcome to the 61st Training Server<br/>
 	To get started, select a role from the group menu and access subsequent menus through ACE Self Interact. You can view more information in the diary.<br/><br/>
 	<br/>
-	“A nation is not soil and flags — it is blood. And blood that does not belong must be spilled, or Vardovia will rot from the inside. I will not hesitate. History does not remember the merciful — it remembers the ones who were strong enough to decide who deserved to remain.”
-	- Leader of WSS (Wolf of the South Slavs): Ivan Radovanović
+	If you arn't struggiling, you aint learning.: Sun Zu...Probably
 	<br/>
 	<br/>
 	There are signs with instructions around the starting base
