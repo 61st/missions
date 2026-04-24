f40472ae4c7d64bedc2e274edba7191713074399
whiteytidys <whiteytidys@gmail.com>
2026-04-24 18:16:07 -0500

    quickfix

--- Operation Events/61stosama.tem_anizay/config/config_welcome.hpp
+++ Operation Events/61stosama.tem_anizay/config/config_welcome.hpp
@@ -1,6 +1,6 @@
 private _message = "
-	<t align='center' size='5' shadow='0'><br/>61st Side Operations</t><br/>
-	Welcome to the 61st Operation Black Eagle<br/>
+	<t align='center' size='5' shadow='0'><br/>Operation Neptune Spear</t><br/>
+	Welcome to the 61st Fun Operation Server<br/>
 	To get started, select a role from the group menu and access subsequent menus through ACE Self Interact. You can view more information in the diary.<br/><br/>
 	<br/>
 	If you are new and have not been assigned to a squad or a sections, Please stand behind the formation, 
