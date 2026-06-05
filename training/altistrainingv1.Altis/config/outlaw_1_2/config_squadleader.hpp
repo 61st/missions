2d92d055babd7a389b85178b8bd2d6f58351a327
whiteytidys <whiteytidys@gmail.com>
2026-06-04 21:45:42 -0500

    altistrainingfix_cury_6426

--- training/altistrainingv1.Altis/config/outlaw_1_2/config_squadleader.hpp
+++ training/altistrainingv1.Altis/config/outlaw_1_2/config_squadleader.hpp
@@ -12,12 +12,12 @@ class squadlead12 {
     icon = "\A3\ui_f\data\map\vehicleicons\iconmanofficer_ca.paa";
 
     defaultLoadout[] = {
-            {"rhs_weap_m4a1_carryhandle_mstock_grip3","","rhsusf_acc_anpeq15_bk","rhsusf_acc_ACOG_USMC",{"ACE_30Rnd_556x45_Stanag_M995_AP_mag",30},{},"rhsusf_acc_grip3"},{"rhs_weap_M136","","","",{},{},""},
+            {"rhs_weap_m4a1_carryhandle_mstock","","rhsusf_acc_anpeq15side","rhsusf_acc_ACOG_USMC",{"ACE_30Rnd_556x45_Stanag_M995_AP_mag",30},{},""},{},
             {"MHS_M17","","","",{"MHS_21rnd_9MM_124FMJ_M17",21},{},""},
-            {"U_tweed_acu_summer_ocp_tuck_trop",{}},
-            {"V_tweed_msv_mk2_1",{{"ItemcTabHCam",1},{"ACE_30Rnd_556x45_Stanag_M995_AP_mag",12,30},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"rhs_mag_m67",4,1},{"SmokeShell",4,1},{"rhs_mag_mk84",4,1},{"MS_Strobe_Mag_1",2,1}}},
-            {"B_simc_US_Molle_sturm_OCP_RTO_wasser",{{"ACE_EntrenchingTool",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"lxim_medbags_FirstAid",1},{"ACM_SAMSplint",5},{"ACE_tourniquet",5},{"ACE_Flashlight_XL50",1},{"ACE_SpraypaintBlue",1},{"lxim_medbags_mopp",1},{"ACM_Paracetamol",2,10},{"tsp_breach_linear_mag",2,1},{"tsp_breach_popper_mag",2,1}}},
-            "H_tweed_ihps_g_tasc_rail","",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
+            {"U_tweed_acu_summer_ocp",{{"ACE_EarPlugs",1},{"ItemcTabHCam",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"",1,0}}},
+            {"V_tweed_msv_mk2_1",{{"ACE_CableTie",4},{"ACE_microDAGR",1},{"ACE_Flashlight_MX991",1},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"SmokeShell",2,1},{"HandGrenade",2,1},{"ACE_30Rnd_556x45_Stanag_M995_AP_mag",14,30}}},
+            {"B_simc_US_Molle_sturm_OCP_RTO_wasser",{{"ACRE_PRC117F",1},{"ACE_EntrenchingTool",1},{"MS_Strobe_Mag_1",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"psq42_blk",1}}},
+            "H_tweed_ihps_g_tasc_rail","G_oak_2_cut",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
             {"ItemMap","ItemAndroid","TFAR_anprc152_1","ItemCompass","ACE_Altimeter",""}
     };
     arsenalitems[] = {
