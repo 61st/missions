3a49f680b4df5f671fbc6b2eaaa26f75ee80b622
whiteytidys <whiteytidys@gmail.com>
2026-08-01 17:44:37 -0500

    curykitedits_cury_8126

--- Operation Retrograde/operation_retrograde_intro_v1.Sehreno/config/config_welcome.hpp
+++ Operation Retrograde/operation_retrograde_intro_v1.Sehreno/config/config_welcome.hpp
@@ -1,10 +1,10 @@
 private _message = "
-	<t align='center' size='5' shadow='0'><br/>Operation Black Eagle</t><br/>
+	<t align='center' size='5' shadow='0'><br/>Operation Retrograde</t><br/>
 	Welcome to the 61st Ops Server<br/>
 	To get started, select a role from the group menu and access subsequent menus through ACE Self Interact. You can view more information in the diary.<br/><br/>
 	<br/>
-	“A nation is not soil and flags — it is blood. And blood that does not belong must be spilled, or Vardovia will rot from the inside. I will not hesitate. History does not remember the merciful — it remembers the ones who were strong enough to decide who deserved to remain.”
-	- Leader of WSS (Wolf of the South Slavs): Ivan Radovanović
+	“War does not determine who is right, only who is left.”
+	- Plato
 	<br/>
 	<br/>
 	There are signs with instructions around the starting base
--- Operation Retrograde/operation_retrograde_intro_v1.Sehreno/config/outlaw_1_2/config_marksman.hpp
+++ Operation Retrograde/operation_retrograde_intro_v1.Sehreno/config/outlaw_1_2/config_marksman.hpp
@@ -11,10 +11,10 @@ class marksman12 {
     defaultLoadout[] = {
             {"rhs_weap_sr25","","rhsusf_acc_anpeq15side","lxim_equipment_optic_DMS",{"rhsusf_20Rnd_762x51_SR25_m993_Mag",20},{},""},{},
             {"MHS_M17","","","",{"MHS_21rnd_9MM_124FMJ_M17",21},{},""},
-            {"U_tweed_acu_summer_ocp",{{"ACE_EarPlugs",1},{"ItemcTabHCam",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"",1,0}}},
-            {"V_tweed_msv_mk2_1",{{"ACE_CableTie",4},{"ACE_microDAGR",1},{"ACE_Flashlight_MX991",1},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"SmokeShell",2,1},{"HandGrenade",2,1},{"rhsusf_20Rnd_762x51_SR25_m993_Mag",13,20}}},
-            {"B_simc_US_Molle_sturm_OCP",{{"ACE_EntrenchingTool",1},{"MS_Strobe_Mag_1",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"psq42_blk",1}}},
-            "H_tweed_ihps_g_tasc_rail","G_oak_2_cut",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
+            {"rhs_uniform_FROG01_wd",{{"ACE_EarPlugs",1},{"ItemcTabHCam",1},{"lxim_medbags_mopp",1},{"lxim_medbags_FirstAid",1},{"ACE_MapTools",1},{"",1,0}}},
+            {"rhsusf_spc_squadleader",{{"ACE_CableTie",4},{"ACE_microDAGR",1},{"ACE_Flashlight_MX991",1},{"MHS_21rnd_9MM_124FMJ_M17",3,21},{"SmokeShell",2,1},{"HandGrenade",2,1},{"rhsusf_20Rnd_762x51_SR25_m993_Mag",13,20}}},
+            {"lxim_backpack_Kitbag_Solid_CoyoteBrown",{{"ACE_EntrenchingTool",1},{"MS_Strobe_Mag_1",1},{"ACE_wirecutter",1},{"ACE_Fortify",1},{"ACE_CableTie",5},{"psq42_blk",1}}},
+            "rhsusf_lwh_helmet_marpatwd","G_oak_2_cut",{"lxim_equipment_Vector_Designator","","","",{"Laserbatteries",1},{},""},
             {"ItemMap","ItemAndroid","TFAR_anprc152_1","ItemCompass","ACE_Altimeter",""}
     };
     // for 1-1
